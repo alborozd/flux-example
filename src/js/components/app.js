43cc231e86c233f080e7d581f42d6149cf9f2cd2
Alexander Borozdyuhin <alborozd@gmail.com>
2016-04-04 14:25:10 +0300

    actions

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,9 +1,13 @@
 var React = require("react");
+var AppActions = require("../actions/app-actions");
 
 var App = React.createClass({
+	clickHandle: function() {
+		AppActions.addItem("some item");
+	},
 	render: function() {
 		return (
-			<h1>application</h1>
+			<h1 onClick={this.clickHandle}>applicatio1</h1>
 		);
 	}
 });
